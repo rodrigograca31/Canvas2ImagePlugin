c46d2efe0abe0de385139c550ce70ca64af2cf06
Adam Taylor <ataylor32@gmail.com>
2019-11-26 13:56:46 -0700

    Retrieve the image path also on iOS
    Credit goes to Michele Costabile <michelecos@yahoo.com>
    https://github.com/devgeeks/Canvas2ImagePlugin/pull/68

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -9,6 +9,7 @@
 
 #import "Canvas2ImagePlugin.h"
 #import <Cordova/CDV.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @implementation Canvas2ImagePlugin
 @synthesize callbackId;
@@ -22,30 +23,25 @@
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
-	NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
-}
+    NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
 
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-{
-    // Was there an error?
-    if (error != NULL)
-    {
-        // Show error message...
-        NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    }
-    else  // No errors
-    {
-        // Show message image successfully saved
-        NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    }
+    UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    // Request to save the image to camera roll
+    [library writeImageToSavedPhotosAlbum:[image CGImage] orientation:ALAssetOrientationRight /*(ALAssetOrientation)[image imageOrientation]*/ completionBlock:^(NSURL *assetURL, NSError *error){
+        if (error) {
+            // Show error message...
+            NSLog(@"ERROR: %@",error);
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+            [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+        } else {
+            // Show message image successfully saved
+            NSLog(@"IMAGE SAVED!");
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: [assetURL absoluteString]];
+            [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+        }
+    }];
+    [library release];
 }
 
 - (void)dealloc
